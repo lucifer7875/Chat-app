34e96996ce81b04b4f7c870bade062efb13ef53d
lucifer7875 <hardiksinhrajput98@gmail.com>
2022-06-07 18:30:02 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,96 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+
+const app = express();
+// const http = require("http");
+// const cors = require("cors");
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+// const server = http.createServer(app);
+
+mongoose.connect(
+    "mongodb://localhost:27017/chatUserDB",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    () => {
+        console.log("DB Connected")
+    })
+
+const userSchema = new mongoose.Schema({
+    userName: "string",
+    firstName: "string",
+    lastName: "string",
+    mobileNumber: "string",
+    // image: "string",
+    password: "string",
+
+})
+
+const User = new mongoose.model("User", userSchema)
+
+// routes
+app.post("/login", (req, res) => {                                   // URL BASE POST APP
+    const { mobileNumber, password } = req.body
+    User.findOne({ mobileNumber: mobileNumber }, (err, user) => {
+        if (user) {
+            if (password === user.password) {
+                res.send({ message: "Login successfull", user: user })
+            } else {
+                res.send({ message: "password did not match" })
+            }
+        } else {
+            res.send({ message: "User not found" })
+        }
+    })
+})
+
+app.post("/register", (req, res) => {                                                            // URL BASE POST APP
+    const { userName, firstName, lastName, mobileNumber, image, password } = req.body
+    User.findOne({ mobileNumber: mobileNumber }, (err, user) => {                                                 // check email
+        if (user) {
+            res.send({ message: "user already registerd" })
+        } else {
+            const user = new User({
+                userName: userName,
+                firstName: firstName,
+                lastName: lastName,
+                mobileNumber: mobileNumber,
+                image: image,
+                password: password
+            })
+            user.save(err => {
+                if (err) {
+                    res.send(err)
+                } else {
+                    res.send({ message: "Successfully Registered , plese Login " })
+                }
+            })
+        }
+    })
+
+})
+
+// Profile Routes
+
+app.get("/profile/:id", async (req, res) => {
+    // console.log("showing data")
+    console.log(req.params.id)
+    console.log("got a request")
+
+    const data = await User.findById(req.params.id)
+    console.log(data)
+    res.send(data)
+    console.log("server request ends here")
+})
+
+
+app.listen(9002, () => {
+    console.log("BE started at port 9002")
+})
